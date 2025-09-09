52e949e7c0db29a2548f9c12aac7c2833861fb55
khoaleq98 <144438481+khoaleq98@users.noreply.github.com>
2025-09-09 15:51:55 +0700

    yda

--- app/table.tsx
+++ app/table.tsx
@@ -123,7 +123,9 @@ export default function UsersTable({ users }: { users: User[] | any[] }) {
           <Modal.Title>Thông tin khách mời: <Bold>{selectedUser ? selectedUser : ''}</Bold> </Modal.Title>
         </Modal.Header>
         <Modal.Body  >
-          <main className="p-0 md:p-5 mx-0 max-w-8xl text-white" style={style} ref={componentRef}>
+          {/* <main className="p-0 md:p-5 mx-0 max-w-8xl text-white" style={style} ref={componentRef}> */}
+                      <main className="p-0 md:p-5 mx-0 max-w-8xl text-white"  ref={componentRef}>
+
             {/* <Row className='custom-height'>
               <Col lg={7} className="mx-0 d-flex flex-column align-items-center justify-content-center font-size-50">
                 <p className='custom-name'  >{`${user?.pre_name} ${user?.name}`}</p>
